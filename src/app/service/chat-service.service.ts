fdd3d4900621ec8f120087ec495ae17f88038067
Poojani Hirunima <you@example.compoojanihirunima@gmail.com>
2025-09-28 14:22:51 +0530

    Chat service code completed

--- src/app/service/chat-service.service.ts
+++ src/app/service/chat-service.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
 
+export interface Message {
+  from: string;
+  text: string;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class ChatServiceService {
+
+  private usersSource = new BehaviorSubject<string[]>(['User1', 'User2', 'User3',
+  users$ = this.usersSource.asObservable();
+
+  private messagesSource = new BehaviorSubject<Message[]>([
+    {from: 'User1', text: 'Hello'},
+    {from: 'User2', text: 'Hello, Hi'},
+    {from: 'User3', text: 'Hello, Bye!!'}
+  ]);
+
+  message$ = this.messagesSource.asObservable();
+
+  sendMessage(from: string, text: string){
+    const current = this.messagesSource.getValue();
+    this.messagesSource.next([current, {from, text }]);
+  }
 
   constructor() { }
 }
